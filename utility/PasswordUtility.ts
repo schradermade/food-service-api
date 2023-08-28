f9105a2c72ff291664ffa71195c90c83f999367d
Nathan Schrader <natecschrader@gmail.com>
2023-08-28 15:29:20 -0700

    add function to generate jwt signature

--- utility/PasswordUtility.ts
+++ utility/PasswordUtility.ts
@@ -1,4 +1,7 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+import { VenderPayload } from '../dto';
+require('dotenv').config()
 
 export const generateSalt = async () => {
     return await bcrypt.genSalt()
@@ -10,4 +13,11 @@ export const generatePassword = async (password: string, salt: string) => {
 
 export const validatePassword = async (enteredPassword: string, savedPassword: string, salt: string) => {
     return await generatePassword(enteredPassword, salt) === savedPassword
+}
+
+export const generateSignature = (payload: VenderPayload) => {
+    return jwt.sign(payload, process.env.APP_SECRET || '', {
+        expiresIn: '1d'
+    })
+
 }
