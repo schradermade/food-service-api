83a01a9974fa15317b3d73eb8a4df7ac3e3b8b37
Nathan Schrader <natecschrader@gmail.com>
2023-08-28 14:57:01 -0700

    add function to validate password

--- utility/PasswordUtility.ts
+++ utility/PasswordUtility.ts
@@ -1,9 +1,13 @@
 import bcrypt from 'bcrypt'
 
-export const GenerateSalt = async () => {
+export const generateSalt = async () => {
     return await bcrypt.genSalt()
 }
 
-export const GeneratePassword = async (password: string, salt: string) => {
+export const generatePassword = async (password: string, salt: string) => {
     return await bcrypt.hash(password, salt);
+}
+
+export const validatePassword = async (enteredPassword: string, savedPassword: string, salt: string) => {
+    return await generatePassword(enteredPassword, salt) === savedPassword
 }
