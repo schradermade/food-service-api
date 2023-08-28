094f796f441d0e242cecd6385995c2fa44d395e7
Nathan Schrader <natecschrader@gmail.com>
2023-08-28 15:30:07 -0700

    add vendor routes

--- routes/VendorRoute.ts
+++ routes/VendorRoute.ts
@@ -1,9 +1,13 @@
 import express, {Request, Response, NextFunction} from 'express'
-import { VendorLogin } from '../controllers';
+import { GetVendorProfile, UpdateVendorProfile, UpdateVendorService, VendorLogin } from '../controllers';
 
 const router = express.Router()
 
 router.post('/login', VendorLogin);
+
+router.get('/profile', GetVendorProfile)
+router.patch('/profile', UpdateVendorProfile);
+router.patch('/service', UpdateVendorService);
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.json({message: 'Hello from Vendor'})
