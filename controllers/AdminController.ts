223ad2334c4a0354dbe7e6327bd2b2e483e7547b
Nathan Schrader <natecschrader@gmail.com>
2023-08-28 13:18:14 -0700

    implement bcrypt/genSalt

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,15 +1,22 @@
 import {Request, Response, NextFunction} from 'express'
 import {CreateVendorInput} from '../dto'
 import { Vendor } from '../models';
+import { GeneratePassword, GenerateSalt } from '../utility';
 
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
     const { name, address, pincode, foodType, email, password, ownerName, phone} = <CreateVendorInput>req.body;
 
     const existingVendor = await Vendor.findOne({email: email})
-    
+
     if (existingVendor !== null) {
         return res.json({"message": "A vendor with that email already exists."})
     }
+
+    // generate salt
+    const salt = await GenerateSalt()
+    const userPassword = await GeneratePassword(password, salt);
+
+    // encrypt the password using the salt
 
     const createdVendor = await Vendor.create({
         name: name,
@@ -17,8 +24,8 @@ export const CreateVendor = async (req: Request, res: Response, next: NextFuncti
         pincode: pincode,
         foodType: foodType,
         email: email,
-        password: password,
-        salt: 'fake salt data',
+        password: userPassword,
+        salt: salt,
         ownerName: ownerName,
         phone: phone,
         rating: 0,
